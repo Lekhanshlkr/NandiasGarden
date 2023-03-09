ddacfd9d85778b37e1651dddb3e2a3d517e66044
Lekhansh Ruprela <lekhanshruprela@MacBook-Pro.local>
2023-03-09 13:58:58 +0530

    Direct Deploy to Beanstalk

--- nandiasgarden/settings.py
+++ nandiasgarden/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-dvxa(%!_(swt8+$a3rsj@4%04blao6&o&!@@667ci21n9#q%@s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".elasticbeanstalk.com","localhost","127.0.0.1"]
 
 
 # Application definition
